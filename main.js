4977c0f8ee79f8ee1da072459affdf073a8a65be
Raxont <157431520+Raxont@users.noreply.github.com>
2024-07-30 12:18:20 -0500

    feat: ✨ seat assignment completed

--- main.js
+++ main.js
@@ -1,71 +1,34 @@
-import { BoletaRepository } from "./js/modules/boleta.js"; // ? Importa el repositorio de boleta
+import { asientoRepository } from "./js/modules/asientos.js"; // ? Importa el repositorio de asiento
 import { ObjectId } from "mongodb"; // ? Importa el constructor de ObjectId de MongoDB
 
 //* Función principal que ejecuta diferentes acciones basadas en el parámetro 'action'
 async function main(action) {
   try {
-    const boletaRepo = new BoletaRepository(); //* Crea una instancia del repositorio de boleta
+    const asientoRepo = new asientoRepository(); //* Crea una instancia del repositorio de asiento
 
-    if (action === "getAll") {
-      //* Si la acción es 'getAll', obtiene todas las boletas
-      const boletas = await boletaRepo.getAllboleta();
-      console.log("Boletas:", boletas);
-    } 
-    else if (action === "add") {
-      //* Si la acción es 'add', agrega una nueva boleta
-      const nuevaBoleta = {
-        identificacion_cliente: 1234567890,//! Solo es permitido un minimo y un maximo de 10 digitos
-        id_lugar: new ObjectId("66a579bb7b00907fab0aee94"), // ! Reemplaza con ObjectId válidos de la base de datos
-        fecha_adquisicion: new Date(), // ! Reemplaza con una fecha valida
-        estado: "en_linea", //! Solo es permitido 'en_linea', 'fisico'
-        id_asiento: []
+    if (action === "getReserva") {
+      //* Si la acción es 'getReserva', permite la selección y reserva de asientos.
+      const informacion = {
+        idAsiento: new ObjectId("66a6d3fa1c9570011db88fdb"), //! Reemplaza con el ID del asiento
+        idLugar: new ObjectId("66a579bb7b00907fab0aee94"), //! Reemplaza con el ID del lugar
+        identificacionCliente: 1234567890 //! Reemplaza con la identificación del cliente
       };
-      const resultadoAgregar = await agregarBoleta(boletaRepo, nuevaBoleta);
-      console.log("Boleta agregada:", resultadoAgregar);
+      const resultadoActualizar = await asientoRepo.updateAsientoInBoleta(informacion);
+      console.log("Asiento actualizado en boleta:", resultadoActualizar);
     } 
-    else if (action === "update") {
-      //* Si la acción es 'update', actualiza la información de una boleta
-      const boletaActualizada = {
-        id: new ObjectId("66a5238f31e93ae393b3e498"), // ! Reemplaza con el ID de la boleta que deseas actualizar
-        estado: "fisico", // ! Reemplaza o agrega la información que desea actualizar
+    else if (action === "returnReserva") {
+      //* Si la acción es 'return', cancela una reserva de asiento ya realizada
+      const informacion = {
+        idAsiento: new ObjectId("66a6d3fa1c9570011db88fdb"), //! Reemplaza con el ID del asiento
+        idLugar: new ObjectId("66a579bb7b00907fab0aee94"), //! Reemplaza con el ID del lugar
+        identificacionCliente: 1234567890 //! Reemplaza con la identificación del cliente
       };
-      const resultadoActualizar = await actualizarBoleta(
-        boletaRepo,
-        boletaActualizada
-      );
-      console.log("Boleta actualizada:", resultadoActualizar);
+      const resultadoActualizar = await asientoRepo.revertAsientoInBoleta(informacion);
+      console.log("Asiento actualizado en boleta:", resultadoActualizar);
     } 
-    else if (action === "delete") {
-      //* Si la acción es 'delete', elimina una boleta
-      const idBoleta = "66a5238f31e93ae393b3e498"; // ! Reemplaza con el ID de la boleta que deseas eliminar
-      const resultadoEliminar = await eliminarBoleta(boletaRepo, idBoleta);
-      console.log("Boleta eliminada:", resultadoEliminar);
-    } 
-    else if (action === "getById") {
-      //* Si la acción es 'getById', obtiene una boleta por ID
-      const idBoleta = "66a5238f31e93ae393b3e498"; // ! Reemplaza con el ID de la boleta que deseas obtener
-      const boleta = await boletaRepo.getboletaById(new ObjectId(idBoleta));
-      console.log("Boleta por ID:", boleta);
-    } 
-    else if (action === "getByCliente") {
-      //* Si la acción es 'getByCliente', obtiene boletas por identificación de cliente y trae la fecha de inicio de cada lugar
-      const identificacionCliente = 1234567890; // ! Reemplaza con la identificación del cliente que deseas buscar
-      const boletasConFechaInicio = await boletaRepo.getBoletasWithFecha_Inicio(
-        identificacionCliente
-      );
-      console.log("Boletas por cliente con fecha de inicio:", boletasConFechaInicio); 
-    }
-    else if (action === "getAsientos") {
-      //* Si la acción es 'getAsientos', permite la consulta de la disponibilidad de asientos en una sala para una proyección específica
-      const idLugar = new ObjectId("66a579bb7b00907fab0aee94"); // ! Reemplaza con la identificación del lugar que deseas buscar asientos disponibles
-      const boletasConFechaInicio = await boletaRepo.getAsientosAvailable(
-        idLugar
-      );
-      console.log("Asientos disponibles:", boletasConFechaInicio); 
-    }
     else {
       console.log(
-        "Acción no válida. Usa 'getAll', 'add', 'update', 'delete', 'getById', 'getByCliente', 'getAsientos'." //! Esta acción no es válida
+        "Acción no válida. Usa 'getReserva', 'returnReserva'." //! Esta acción no es válida
       );
     }
   } catch (error) {
@@ -73,90 +36,26 @@ async function main(action) {
   }
 }
 
-//* Función para agregar una boleta
-async function agregarBoleta(boletaRepo, boleta) {
-  try {
-    const resultado = await boletaRepo.addboleta(boleta);
-    return resultado;
-  } catch (error) {
-    console.error("Error agregando boleta:", error); // ! Manejo de errores al agregar una boleta
-  }
-}
-
-//* Función para actualizar una boleta
-async function actualizarBoleta(boletaRepo, actualizado) {
-  try {
-    const resultado = await boletaRepo.updateboleta(actualizado);
-    return resultado;
-  } catch (error) {
-    console.error("Error actualizando boleta:", error); // ! Manejo de errores al actualizar una boleta
-  }
-}
-
-//* Función para eliminar una boleta
-async function eliminarBoleta(boletaRepo, id) {
-  try {
-    const resultado = await boletaRepo.deleteboleta(id);
-    return resultado;
-  } catch (error) {
-    console.error("Error eliminando boleta:", error); // ! Manejo de errores al eliminar una boleta
-  }
-}
-
-const action = "getAsientos"; //* Acción por defecto el cual debe de ser modificando según la necesidad
+const action = "returnReserva"; //* Acción por defecto el cual debe de ser modificando según la necesidad
 main(action); // * Ejecuta la función principal con la acción definida
 /**
- *  //* Valor que muestra las boletas por identificación de cliente
- * @returns Boletas por cliente con fecha de inicio: [
-              {
-                identificacion_cliente: 1234567890,
-                id_lugar: new ObjectId('66a579bb7b00907fab0aee94'),
-                fecha_adquisicion: 2024-07-29T21:07:55.646Z,
-                estado: 'en_linea',
-                id_asiento: [],
-                fechaHora_pelicula: 2024-08-01T10:00:00.000Z
+ *  //* Valor que muestra al realizar una reserva de un asiento
+ * @returns Asiento actualizado en boleta: {
+              message: 'Asiento actualizado correctamente en la boleta.',
+              movimientos: {
+                asiento: 'idLugar 66a579bb7b00907fab0aee94 eliminado de idAsiento 66a6d3fa1c9570011db88fdb',
+                boleta: 'idAsiento 66a6d3fa1c9570011db88fdb agregado a la boleta del cliente 1234567890'    
               }
-            ]
+            }
  */
 
 /**
- *  //* Valor que muestra la disponibilidad de asientos en una sala
- * @returns Asientos disponibles: [
-              {
-                _id: new ObjectId('66a804abdf6d8860acf5baf4'),
-                tipo_fila: 'premier',
-                codigo: 'A01',
-                incremento: 10
-              },
-              {
-                _id: new ObjectId('66a804abdf6d8860acf5baf4'),
-                tipo_fila: 'premier',
-                codigo: 'A02',
-                incremento: 10
-              },
-              {
-                _id: new ObjectId('66a804abdf6d8860acf5baf4'),
-                tipo_fila: 'premier',
-                codigo: 'A03',
-                incremento: 10
-              },
-              {
-                _id: new ObjectId('66a804abdf6d8860acf5baf4'),
-                tipo_fila: 'general',
-                codigo: 'B01',
-                incremento: 0
-              },
-              {
-                _id: new ObjectId('66a804abdf6d8860acf5baf4'),
-                tipo_fila: 'general',
-                codigo: 'B02',
-                incremento: 0
-              },
-              {
-                _id: new ObjectId('66a804abdf6d8860acf5baf4'),
-                tipo_fila: 'general',
-                codigo: 'B03',
-                incremento: 0
+ *  //* Valor que muestra al cancelar una reserva de un asiento
+ * @returns Asiento actualizado en boleta: {
+              message: 'Asiento revertido correctamente en la boleta.',
+              movimientos: {
+                asiento: 'idLugar 66a579bb7b00907fab0aee94 agregado a idAsiento 66a6d3fa1c9570011db88fdb',
+                boleta: 'idAsiento 66a6d3fa1c9570011db88fdb eliminado de la boleta del cliente 1234567890'
               }
-            ]
+            }
  */
