e31bd7b271a91cc9a0a8b37261c9ad7970da204b
Raxont <157431520+Raxont@users.noreply.github.com>
2024-09-02 01:14:16 -0500

    feat: :sparkles: The user's name is already displayed in the location section

--- public/js/module/lugar.js
+++ public/js/module/lugar.js
@@ -1,46 +1,82 @@
 // Espera a que el contenido del documento se haya cargado completamente
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Función para obtener la identificación desde el servidor
+    async function obtenerIdentificacion() {
+        try {
+            // Obtiene la identificación del cliente desde la API
+            const response = await fetch('/api/config');
+            const data = await response.json();
     
+            // Asegúrate de que la identificación se obtenga correctamente y sea un número
+            const cliente = Number(data.identificacion);
+    
+            return cliente;
+        } catch (error) {
+            console.error('Error al obtener la identificación:', error);
+            return null;  // Devuelve null en caso de error para manejarlo adecuadamente
+        }
+    }
+
+    // Función para obtener la información del cliente usando la identificación
+    function obtenerInfoCliente(identificacion) {
+        return fetch(`http://localhost:3000/cliente/info/${identificacion}`)
+            .then(response => response.json())
+            .then(data => {return data.resultado})
+            .catch(error => console.error('Error al obtener la información del cliente:', error));
+    }
+
+    // Función para actualizar el título principal con el nombre del cliente
+    function actualizarTituloPrincipal(cliente) {
+        const mainTitleElement = document.querySelector('.main-title small');
+        if (mainTitleElement) {
+            mainTitleElement.textContent = `Hi, ${cliente.nombre}!`;
+        }
+    }
+
+    // Función principal para inicializar la página
+    async function inicializarPagina() {
+        const gente=await obtenerIdentificacion()
+        const respuesta = await obtenerInfoCliente(gente);
+        actualizarTituloPrincipal(respuesta);
+    }
+        
+
+    // Llama a la función para inicializar la página
+    inicializarPagina();
+
     // Función para inicializar y configurar Flickity
     function initializeFlickity() {
         var flickityElement = document.querySelector('.gallery');
 
-        // Verifica si el elemento de Flickity existe
         if (!flickityElement) {
             console.log('No se encontró el elemento de Flickity');
             return;
         }
 
-        // Si ya existe una instancia de Flickity, la destruye para reinicializar
         if (flickityElement.flickityInstance) {
             flickityElement.flickityInstance.destroy();
             console.log('Flickity destruido para re-inicializar');
         }
 
-        // Añade una clase para aplicar estilos específicos
         flickityElement.classList.add('js-flickity');
 
-        // Inicializa Flickity con opciones de configuración
         var flickityInstance = new Flickity(flickityElement, {
-            wrapAround: true,              // Permite el desplazamiento circular
-            friction: 0.2,                 // Configura la fricción del desplazamiento
-            cellAlign: 'center',           // Alinea las celdas al centro
-            autoPlay: 4000,                // Configura la reproducción automática cada 4 segundos
-            pauseAutoPlayOnHover: true,    // Pausa la reproducción automática al pasar el ratón por encima
-            prevNextButtons: false         // Desactiva los botones de navegación
+            wrapAround: true,
+            friction: 0.2,
+            cellAlign: 'center',
+            autoPlay: 4000,
+            pauseAutoPlayOnHover: true,
+            prevNextButtons: false
         });
 
-        // Asigna la instancia de Flickity al elemento para referencia futura
         flickityElement.flickityInstance = flickityInstance;
 
-        // Ajusta la altura del viewport de Flickity
         var viewport = flickityInstance.viewport;
-
         if (viewport) {
             viewport.style.height = '100%';
         }
 
-        // Forza un redibujo de la página con un evento de resize
         window.dispatchEvent(new Event('resize'));
 
         return flickityInstance;
@@ -65,19 +101,16 @@ document.addEventListener('DOMContentLoaded', function () {
     // Función para cargar y mostrar películas en la galería
     function loadMovies() {
         const today = new Date();
-        const fechaInicio = today.toISOString().split('T')[0]; // Fecha de hoy en formato YYYY-MM-DD
+        const fechaInicio = today.toISOString().split('T')[0];
 
-        // Calcula la fecha final que es 14 días después de hoy
         const fechaFinal = new Date(today);
         fechaFinal.setDate(today.getDate() + 14);
         const fechaFin = fechaFinal.toISOString().split('T')[0];
-        
-        // Realiza una solicitud para obtener las películas entre las fechas especificadas
+
         fetch(`/lugar/lugaresPorFecha?fechaInicioFiltro=${encodeURIComponent(fechaInicio)}&fechaFinFiltro=${encodeURIComponent(fechaFin)}`)
-        .then(response => response.json())
+            .then(response => response.json())
             .then(data => {
                 const galeriaContainer = document.getElementById('galeria');
-                // Muestra las películas en la galería
                 galeriaContainer.innerHTML = data.data.map(pelicula => `
                     <div class="gallery-cell" data-id="${pelicula._id}">
                         <img src="${pelicula.img}" alt="${pelicula.titulo}">
@@ -86,7 +119,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                 `).join('');
 
-                // Añadir redirección al hacer clic en una película
                 galeriaContainer.querySelectorAll('.gallery-cell').forEach(element => {
                     element.addEventListener('click', function () {
                         const peliculaId = this.getAttribute('data-id');
@@ -106,30 +138,21 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.error('Error:', error));
     }
 
-    // Llama a la función para cargar las películas
-    loadMovies();
-});
-
-// Espera a que el contenido del documento se haya cargado completamente
-document.addEventListener('DOMContentLoaded', function () {
     // Función para cargar y mostrar películas que se estrenarán pronto
     function loadMovies2() {
         const today = new Date();
         const fechaInicial = new Date(today);
         fechaInicial.setDate(today.getDate() + 14);
-        const fechaInicio = fechaInicial.toISOString().split('T')[0]; // Fecha de 14 días a partir de hoy en formato YYYY-MM-DD
+        const fechaInicio = fechaInicial.toISOString().split('T')[0];
 
-        // Calcula la fecha final que es 100 días después de hoy
         const fechaFinal = new Date(today);
         fechaFinal.setDate(today.getDate() + 100);
         const fechaFin = fechaFinal.toISOString().split('T')[0];
-        
-        // Realiza una solicitud para obtener las películas entre las fechas especificadas
+
         fetch(`/lugar/lugaresPorFecha?fechaInicioFiltro=${encodeURIComponent(fechaInicio)}&fechaFinFiltro=${encodeURIComponent(fechaFin)}`)
-        .then(response => response.json())
+            .then(response => response.json())
             .then(data => {
                 const galeriaContainer = document.getElementById('movie-soon');
-                // Muestra las películas que se estrenarán pronto
                 galeriaContainer.innerHTML = data.data.map(pelicula => `
                     <div class="movie-comming" data-id="${pelicula._id}">
                         <img src="${pelicula.img}" alt="${pelicula.titulo}">
@@ -140,7 +163,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                 `).join('');
 
-                // Añadir redirección al hacer clic en una película
                 galeriaContainer.querySelectorAll('.movie-comming').forEach(element => {
                     element.addEventListener('click', function () {
                         const peliculaId = this.getAttribute('data-id');
@@ -150,7 +172,8 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             .catch(error => console.error('Error:', error));
     }
-    
-    // Llama a la función para cargar las películas que se estrenarán pronto
+
+    // Llama a las funciones para cargar las películas
+    loadMovies();
     loadMovies2();
 });
